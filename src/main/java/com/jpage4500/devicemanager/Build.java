8d4c6fbf2fe360f1ba6da570d7d02fdd2a66a5c3
Joe Page <jpage4500@gmail.com>
2023-07-26 15:23:36 -0400

    - don't package JRE with app (won't work with both arm and x86 macs)

--- src/main/java/com/jpage4500/devicemanager/Build.java
+++ src/main/java/com/jpage4500/devicemanager/Build.java
@@ -1,6 +1,6 @@
 package com.jpage4500.devicemanager;
 
 public class Build {
-    public static final String versionName = "1.0.30";
-    public static final Integer versionCode = 30;
+    public static final String versionName = "1.0.32";
+    public static final Integer versionCode = 32;
 }
