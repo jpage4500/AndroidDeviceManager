d32608dbf454ebe5cfebe01c28daed66118b4b60
Joe Page <jpage4500@gmail.com>
2024-03-20 15:04:30 -0400

    - minor

--- src/main/java/com/jpage4500/devicemanager/Build.java
+++ src/main/java/com/jpage4500/devicemanager/Build.java
@@ -1,6 +1,6 @@
 package com.jpage4500.devicemanager;
 
 public class Build {
-    public static final String versionName = "1.0.45";
-    public static final Integer versionCode = 45;
+    public static final String versionName = "1.0.50";
+    public static final Integer versionCode = 50;
 }
